c56f4a9953db9b0b1f90baca6eda3829375183c0
stfftw <101828080+stfftw@users.noreply.github.com>
2022-12-29 22:44:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ tar -xzvf yay.tar.gz
 cd yay
 makepkg -si
 
+echo "\033[[1;45mGo grab a drink and some popcorn, it will take at least 20 minutes after you press yes\033[m"
+
 yay -S picom-ibhagwan-git adwaita-dark obmenu
 
 # Download conky configuration files
@@ -33,34 +35,27 @@ echo -e "[Settings]\ngtk-application-prefer-dark-theme=1" > ~/.config/gtk-3.0/se
 # Set gtk-application-prefer-dark-theme option for GTK 4.0
 echo -e "[Settings]\ngtk-application-prefer-dark-theme=1" > ~/.config/gtk-4.0/settings.ini
 
-# Set Fresh-Theme in rc.xml
-echo -e "<!-- Themes -->\n<theme>Fresh-Theme</theme>" > ~/.config/openbox/rc.xml
-
-# Reconfigure openbox
-openbox --reconfigure
-
-# Set lightdm-gtk-greeter as the default greeter
-sudo sed -i 's/^#greeter-session=.*/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
-
 # Download Openbox configuration files
 mkdir -p ~/.config/openbox
 wget -O ~/.config/openbox/autostart https://github.com/stfftw/myarchrice/raw/main/openbox/autostart
 wget -O ~/.config/openbox/menu.xml https://github.com/stfftw/myarchrice/raw/main/openbox/menu.xml
 wget -O ~/.config/openbox/rc.xml https://github.com/stfftw/myarchrice/raw/main/openbox/rc.xml
+
+# Download nitrogen configuration files
 mkdir -p ~/.config/nitrogen
 wget -O ~/.config/nitrogen/bg-saved.cfg https://github.com/stfftw/myarchrice/raw/main/nitrogen/bg-saved.cfg
 wget -O ~/.config/nitrogen/nitrogen.cfg https://github.com/stfftw/myarchrice/raw/main/nitrogen/nitrogen.cfg
+
+# Download kitty configuration files
 mkdir -p ~/.config/kitty
 wget -O ~/.config/kitty/kitty.conf https://github.com/stfftw/myarchrice/raw/main/kitty/kitty.conf
+
 # Download Tint2 configuration file
 mkdir -p ~/.config/tint2
 wget -O ~/.config/tint2/tint2rc https://github.com/stfftw/myarchrice/raw/main/tint2/tint2rc
 
-
-# Create the picom directory
-mkdir -p ~/.config/picom
-
 # Download picom.conf
+mkdir -p ~/.config/picom
 wget -O ~/.config/picom/picom.conf https://github.com/stfftw/myarchrice/raw/main/picom/picom.conf
 
 # Download and place lightdm.conf and lightdm-gtk-greeter.conf
@@ -68,11 +63,9 @@ sudo mkdir -p /etc/lightdm
 sudo wget -O /etc/lightdm/lightdm.conf https://github.com/stfftw/myarchrice/raw/main/lightdm/lightdm.conf
 sudo wget -O /etc/lightdm/lightdm-gtk-greeter.conf https://github.com/stfftw/myarchrice/raw/main/lightdm/lightdm-gtk-greeter.conf
 
-# Download and place adwaita-d.webp
+# Download and place adwaita-d.webp and Archlinux.png
 sudo mkdir -p /usr/backgrounds
 sudo wget -O /usr/backgrounds/adwaita-d.webp https://github.com/stfftw/myarchrice/blob/main/backgrounds/adwaita-d.webp?raw=true
-
-# Download and place Archlinux.png
 sudo wget -O /usr/backgrounds/Archlinux.png https://github.com/stfftw/myarchrice/raw/main/backgrounds/Archlinux.png
 
 # Download and place 686998.jpg and adwaita-d.png
@@ -82,3 +75,10 @@ wget -O ~/THEMES/adwaita-d.png https://github.com/stfftw/myarchrice/raw/main/THE
 
 # Download and place Fresh-Theme.obt
 wget -O ~/THEMES/Fresh-Theme.obt https://github.com/stfftw/myarchrice/blob/main/THEMES/Fresh-Theme.obt?raw=true
+
+# Enable lightdm
+sudo systemctl enable lightdm.service
+
+# Done
+echo "\033[[1;32mDone\033[m"
+echo "Restart by typing \033[[1;46msudo reboot now\033[m"
