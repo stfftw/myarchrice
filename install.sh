52a5e2630dcc3d26de1f89f85aed7ffd71bc2a45
stfftw <101828080+stfftw@users.noreply.github.com>
2022-12-29 21:06:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,6 +60,7 @@ mkdir -p ~/.config/picom
 wget -O ~/.config/picom/picom.conf https://github.com/stfftw/myarchrice/raw/main/picom/picom.conf
 
 # Download and place lightdm.conf and lightdm-gtk-greeter.conf
+mkdir -p /etc/lightdm
 sudo wget -O /etc/lightdm/lightdm.conf https://github.com/stfftw/myarchrice/raw/main/lightdm/lightdm.conf
 sudo wget -O /etc/lightdm/lightdm-gtk-greeter.conf https://github.com/stfftw/myarchrice/raw/main/lightdm/lightdm-gtk-greeter.conf
 
@@ -67,9 +68,11 @@ sudo wget -O /etc/lightdm/lightdm-gtk-greeter.conf https://github.com/stfftw/mya
 sudo wget -O /usr/backgrounds/adwaita-d.webp https://github.com/stfftw/myarchrice/blob/main/backgrounds/adwaita-d.webp?raw=true
 
 # Download and place Archlinux.png
+mkdir -p /usr/backgrounds
 sudo wget -O /usr/backgrounds/Archlinux.png https://github.com/stfftw/myarchrice/raw/main/backgrounds/Archlinux.png
 
 # Download and place 686998.jpg and adwaita-d.png
+mkdir -p /home/THEMES
 wget -O /home/THEMES/686998.jpg https://github.com/stfftw/myarchrice/raw/main/THEMES/686998.jpg
 wget -O /home/THEMES/adwaita-d.png https://github.com/stfftw/myarchrice/raw/main/THEMES/adwaita-d.png
 
